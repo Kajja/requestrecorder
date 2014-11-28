7ef13c962e5e006d242859d488157be4b68da65b
Mikael Feuk <mikael.feuk@yahoo.se>
2014-11-28 23:12:19 +0100

    Removed whitespace

--- src/Recorder/RequestDatabase.php
+++ src/Recorder/RequestDatabase.php
@@ -9,84 +9,84 @@ use Mos\Database\CDatabaseBasic;
  * to be able to better suit the RequestRecord class.
  *
  */
- class RequestDatabase extends CDatabaseBasic implements IDatabaseHandler
- {
+class RequestDatabase extends CDatabaseBasic implements IDatabaseHandler
+{
 
- 	private $table; // Database table name for the records.
+    private $table; // Database table name for the records.
 
- 	/**
- 	 *
- 	 * @param array with setup options
- 	 */
- 	public function __construct($options = [])
- 	{
- 		parent::__construct($options);
+    /**
+     *
+     * @param array with setup options
+     */
+    public function __construct($options = [])
+    {
+        parent::__construct($options);
 
- 		$this->table = 'request';
- 	}
-
-
- 	/**
-	 * Insert a record in the database.
-	 *
-	 * @param array with the column names where the values is to be inserted
-	 * @param array with the values
-	 *
-	 * @return void
-	 */
- 	public function insertRecord($columns, $values)
- 	{
- 		$this->createTableIfNotExists();
-
- 		parent::insert($this->table, $columns, $values);
- 		$this->execute();
- 	}
+        $this->table = 'request';
+    }
 
 
- 	/**
- 	 * Create a table for the records in the database if it
- 	 * does not exist.
- 	 *
- 	 * @return void
- 	 */
- 	public function createTableIfNotExists()
- 	{
-		$this->createTable("IF NOT EXISTS $this->table", [
-			'session'	=> ['varchar(20)'],
-			'id' 		=> ['integer', 'primary key', 'not null', 'auto_increment'],
-			'uri' 		=> ['varchar(2048)'],
-			'method'	=> ['varchar(20)'],
-			'date'		=> ['datetime']
-			]);
+    /**
+     * Insert a record in the database.
+     *
+     * @param array with the column names where the values is to be inserted
+     * @param array with the values
+     *
+     * @return void
+     */
+    public function insertRecord($columns, $values)
+    {
+        $this->createTableIfNotExists();
 
-		$this->execute();
- 	}
+        parent::insert($this->table, $columns, $values);
+        $this->execute();
+    }
 
 
- 	/**
-	 * Get all the records from the records database table.
-	 *
-	 * @return array with all the records in the database
-	 */
- 	public function getAllRecords()
- 	{
- 		$this->select()->from($this->table)
- 			->orderBy('session DESC, id DESC');
- 		$this->execute();
+    /**
+     * Create a table for the records in the database if it
+     * does not exist.
+     *
+     * @return void
+     */
+    public function createTableIfNotExists()
+    {
+        $this->createTable("IF NOT EXISTS $this->table", [
+            'session'   => ['varchar(20)'],
+            'id'        => ['integer', 'primary key', 'not null', 'auto_increment'],
+            'uri'       => ['varchar(2048)'],
+            'method'    => ['varchar(20)'],
+            'date'      => ['datetime']
+            ]);
 
- 		return $this->fetchAll();
- 	}
+        $this->execute();
+    }
 
 
- 	/**
-	 * Delete all records in the records database table.
-	 *
-	 * @return void
-	 */
- 	public function deleteAll()
- 	{
- 		$this->delete($this->table);
- 		$this->execute();
- 	}
+    /**
+     * Get all the records from the records database table.
+     *
+     * @return array with all the records in the database
+     */
+    public function getAllRecords()
+    {
+        $this->select()->from($this->table)
+            ->orderBy('session DESC, id DESC');
+        $this->execute();
 
- }
+        return $this->fetchAll();
+    }
+
+
+    /**
+     * Delete all records in the records database table.
+     *
+     * @return void
+     */
+    public function deleteAll()
+    {
+        $this->delete($this->table);
+        $this->execute();
+    }
+
+}
